75f710ff3966c4bbd9c12c91a1a7193730ccf905
Jonathan Fernandes <xjhofernandes@gmail.com>
2020-06-17 02:14:04 -0300

    Adicionando pequenas funcionalidades

--- js/index.js
+++ js/index.js
@@ -1,17 +1,39 @@
 
-//Menu toggle-effect
-$(document).ready(function(){
-    $(".menu-icon").on("click",function(){
-      $("nav ul").toggleClass("showing");
-    });
-  });
+// //Menu toggle-effect
+// $(document).ready(function(){
+//     $(".menu-icon").on("click",function(){
+//       $("nav ul").toggleClass("showing");
+//     });
+//   });
   
-  //Scrolling Effect
-  $(window).on('scroll', function(){
-    if($(window).scrollTop()) {
-      $('nav').addClass('black');
-    }
-    else{
-      $('nav').removeClass('black')
-    }
+//   //Scrolling Effect
+//   $(window).on('scroll', function(){
+//     if($(window).scrollTop()) {
+//       $('nav').addClass('black');
+//     }
+//     else{
+//       $('nav').removeClass('black')
+//     }
+//   })
+
+
+
+function teste (){
+  a = document.getElementById('jsonResult');
+
+  var b = '';
+  
+  fetch('https://ragnarokapi.herokuapp.com/api/monster/1247')
+  .then(function(response) {
+    return response.json();
   })
+  .then(function(myBlob) {
+    var objectURL = JSON.stringify(myBlob, undefined, 4);
+    b = objectURL;
+    a.value = b;
+  }); 
+
+  
+
+}
+
