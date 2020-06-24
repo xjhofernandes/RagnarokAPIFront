6dfb45674a29a9be32337db740728b66409c6126
Jonathan Fernandes <xjhofernandes@gmail.com>
2020-06-24 00:55:06 -0300

    Comit

--- js/index.js
+++ js/index.js
@@ -1,39 +1,14 @@
+function FazerRequisição (){
+  request = document.getElementById('request');
+  resultado = document.getElementById('jsonResult');
 
-// //Menu toggle-effect
-// $(document).ready(function(){
-//     $(".menu-icon").on("click",function(){
-//       $("nav ul").toggleClass("showing");
-//     });
-//   });
-  
-//   //Scrolling Effect
-//   $(window).on('scroll', function(){
-//     if($(window).scrollTop()) {
-//       $('nav').addClass('black');
-//     }
-//     else{
-//       $('nav').removeClass('black')
-//     }
-//   })
-
-
-
-function teste (){
-  a = document.getElementById('jsonResult');
-
-  var b = '';
-  
-  fetch('https://ragnarokapi.herokuapp.com/api/monster/1002')
+  fetch('https://ragnarokapi.herokuapp.com/' + request.value)
   .then(function(response) {
     return response.json();
   })
   .then(function(myBlob) {
     var objectURL = JSON.stringify(myBlob, undefined, 4);
-    b = objectURL;
-    a.value = b;
+    resultado.value = objectURL;
   }); 
-
-  
-
 }
 
